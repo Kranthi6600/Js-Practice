a0d3e66ddea584e1477cbb1e590388f237dff463
kranthi <kranthichintu5324@gmail.com>
2025-08-04 00:18:08 +0530

    updated

---
+++ js/Scope.js
@@ -0,0 +1,46 @@
+// let a = 299;  //this is global scope
+
+// function localScope() {
+//     let a = 20  // this is local scope
+// }
+
+
+// nested scope
+
+
+// function one() {
+//     const name = 'spidey'
+
+//     function two() {
+//         const website = ' youtube'
+//         console.log(name + website)
+//     }
+//     two()
+// }
+
+// one()
+
+// const user = {
+//     name: 'spidey',
+//     isLoggedIn: true
+// }
+
+// const cart = {
+//     item1: 'thumbs up',
+//     item2: 'vodka',
+//     item3: 'apple'
+// }
+
+// const totalPrice = {
+//     amount: 299
+// }
+
+// if (user.isLoggedIn) {
+//     console.log(`${user.name} is logged in`)
+//     if (cart) {
+//         console.log(`Sir in your cart you have ${cart.item1}, ${cart.item2}, ${cart.item3}`);
+//         console.log(totalPrice.amount)
+//     }
+// } else {
+//     console.log(`${user.name} please do log in`)
+// }
