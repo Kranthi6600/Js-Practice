5e724a1b6996c5fb8c3517a6d5a0b03d960740b0
kranthi <kranthichintu5324@gmail.com>
2025-08-03 21:05:54 +0530

    updated

---
+++ js/Functions.js
@@ -0,0 +1,33 @@
+// function myName() {
+//     console.log('spidey')
+// }
+
+// myName()
+
+// function add2Num(num1, num2) {
+//     console.log(num1 + num2)
+// }
+
+// add2Num(3, 4)
+
+// function add2Nums(num1, num2) {
+//     return console.log(num1 + num2)
+// }
+
+// add2Nums(3, 5)
+
+// function userLogin(user) {
+//     return console.log(`${user} has logged in successfully`)
+// }
+
+// userLogin('spidey')
+
+
+// const arr = [1,2,3,4,5]
+
+// function secondValue(two) {
+//     return two[1]
+// }
+
+// console.log(secondValue(arr))
+
