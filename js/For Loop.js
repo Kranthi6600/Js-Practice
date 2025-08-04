b1c22262b1bfca591f0c8fbb661a7688957a7620
kranthi <kranthichintu5324@gmail.com>
2025-08-04 22:47:41 +0530

    update

---
+++ js/For Loop.js
@@ -0,0 +1,38 @@
+// for (let i = 0; i <= 10; i++) {
+//     const element = i;
+//     if (i === 5) {
+//         console.log('5 is detected')
+//     }
+//     console.log(element)
+// }
+
+
+// for (let i = 0; i <= 10; i++) {
+//     const element = i;
+//     if (i === 5) {
+//         console.log('5 is detected')
+//         break // breaks the flow and never prints after this even including 5 wherein normal case we able to see both the 5 is detected and also number 5
+//     }
+//     console.log(element)
+// }
+
+// for (let i = 0; i <= 10; i++) {
+//     const element = i;
+//     if (i === 5) {
+//         console.log('5 is detected')
+//         continue // continues after 5 and never prints number 5 wherein normal case we able to see both 5 and '5 is detected' 
+//     }
+//     console.log(element)
+// }
+
+
+
+// for (let i = 0; i <= 5; i++) {
+//     console.log('')
+//     console.log(`${i} table`);
+//     console.log('')
+//     for (let j = 0; j <= 5; j++) {
+//         console.log(`${i} * ${j} = ${i*j}`)
+//     }
+    
+// }
